7d0a2fd8addb2a2f897e4a269f4de21c19bf98bc
--get-all <caio.fonseca@brandlovrs.com>
2023-12-03 18:04:14 -0400

    chore: adds script

---
+++ src/main/script.ts
@@ -0,0 +1,15 @@
+// import './config/moduleAlias'
+// import { initializeApp } from '@/main/app'
+// import { FirebaseRepositoryFactory } from '@/main/factories/repositories'
+
+// const db = FirebaseRepositoryFactory.getInstance().make().db
+// const ref = db.collection('ref')
+
+// initializeApp()
+
+// const script = async () => {
+  
+
+// }
+
+// script()
